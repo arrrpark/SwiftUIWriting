6a26a96632a94057e2b97c0ac62b4bdda388480c
arrr <arrrpark@icloud.com>
2022-05-01 12:29:47 +0100

    cornorradius fix

--- WordCounter/extensions/View+Extension.swift
+++ WordCounter/extensions/View+Extension.swift
@@ -10,5 +10,5 @@ import SwiftUI
 extension View {
     func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
         ModifiedContent(content: self, modifier: CornerRadiusStyle(radius: radius, corners: corners))
-    }R
+    }
 }
